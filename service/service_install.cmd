8171a549a369fec0de0cce31caa0b6f865e48577
Arhipov <sergey.arhipov@arcelormittal.com>
2020-08-27 10:20:39 +0300

    added script install/remove service

---
+++ service/service_install.cmd
@@ -0,0 +1,60 @@
+@ECHO OFF
+CLS
+
+set NSSM=C:\bin\language\perl\perl\bin\nssm_64.exe
+set SERVICE_NAME="scheduler"
+set APP_PATH=C:\bin\language\perl\perl\bin\perl.exe
+set APP_DIR=C:\bin\daemon\scheduler
+set APP_PARAMETERS="scheduler.pl"
+set APP_DISPLAYNAME="ArcelorMittal.%SERVICE_NAME%"
+set APP_DESCRIPTION="планировщик заданий для MS SQL"
+set APP_ROTATEBYTES=26214400
+
+
+:menu
+ECHO.
+ECHO ...............................................
+ECHO PRESS 1, 2, 3 or 4 to EXIT.
+ECHO ...............................................
+ECHO.
+ECHO 1 - Install Service
+ECHO 2 - Remove Service
+ECHO 3 - Set User Name and Password
+ECHO 4 - EXIT
+ECHO.
+
+SET /P M="Type 1, 2, 3 or 4 then press ENTER: "
+IF %M%==1 GOTO install
+IF %M%==2 GOTO remove
+IF %M%==3 GOTO user
+IF %M%==4 GOTO eof
+
+:install
+%NSSM% install %SERVICE_NAME% %APP_PATH%
+%NSSM% set %SERVICE_NAME% AppDirectory %APP_DIR%
+%NSSM% set %SERVICE_NAME% AppParameters %APP_PARAMETERS%
+
+%NSSM% set %SERVICE_NAME% DisplayName %APP_DISPLAYNAME%
+%NSSM% set %SERVICE_NAME% Description %APP_DESCRIPTION%
+%NSSM% set %SERVICE_NAME% Start SERVICE_AUTO_START
+
+%NSSM% set %SERVICE_NAME% AppStderr %APP_DIR%\logs\\%SERVICE_NAME%.err.log
+
+%NSSM% set %SERVICE_NAME% AppRotateFiles 1
+%NSSM% set %SERVICE_NAME% AppRotateOnline 1
+%NSSM% set %SERVICE_NAME% AppRotateBytes %APP_ROTATEBYTES%
+GOTO menu
+
+:remove
+%NSSM% remove %SERVICE_NAME% confirm
+GOTO menu
+
+:user
+ECHO.
+set /P USER="Enter User Name: "
+set /p PASS="Enter Password: "
+%NSSM% set %SERVICE_NAME% ObjectName "%USER%" "%PASS%"
+%NSSM% get %SERVICE_NAME% ObjectName
+GOTO menu
+
+:eof
